5767bf1a6044819a7cec5c058c6c5df9ce19a09f
Emman Oduor <wyllademanu003@gmail.com>
2025-03-12 12:12:46 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+function ourReduce(arr, callback) {
+    let accumulator = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        accumulator = callback(accumulator, arr[i]);
+    }
+    return accumulator;
+}
+
+const totalBatteries = ourReduce(batteryBatches, (accumulator, currentValue) => accumulator + currentValue);
+
+module.exports = totalBatteries;
+
+console.log('Total battery', totalBatteries);
